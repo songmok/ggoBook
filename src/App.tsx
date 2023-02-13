c5f80ace2197e2fc2c7dd1b71326213c560d53b4
devking <brand0610@naver.com>
2023-02-13 16:03:23 +0900

    login 페이지 작업중

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
 import Footer from "components/common/footer/Footer";
 import Header from "components/common/header/Header";
 import Event from "pages/event/Event";
+import Login from "pages/login/Login";
 import MyPage from "pages/myPage/MyPage";
+import NotFound from "pages/notFound/NotFound";
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
       <Router>
-        <Header />
         <Routes>
-          <Route />
-          <Route path="/event" element={<Event />} />
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/home"
+            element={
+              <>
+                <Header />
+              </>
+            }
+          />
+          <Route
+            path="/event"
+            element={
+              <>
+                <Header />
+                <Event />
+                <Footer />
+              </>
+            }
+          />
           <Route path="/myPage" element={<MyPage />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
-        <Footer />
       </Router>
     </>
   );
---
+++ src/pages/notFound/style/NotFoundCss.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const NotFoundCss = styled.section``;
+
+export default NotFoundCss;
