a7a87ade5a868f7ac7f20b99353885116744e6b9
devking <brand0610@naver.com>
2023-02-14 14:29:05 +0900

    fix

---
+++ src/components/common/header/Header.js
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { Link, NavLink } from "react-router-dom";
+import HeaderCss from "./style/HeaderCss";
+import { navheader } from "./data/headjson";
+const Header = () => {
+  useEffect(() => {}, []);
+  if (window.location.pathname === "/login") return null;
+  return (
+    <HeaderCss>
+      <div>
+        <Link to="/login">Project SB</Link>
+      </div>
+      <div>
+        <ul>
+          {navheader.map((v, i) => {
+            return (
+              <li key={i}>
+                <NavLink
+                  to={v.to}
+                  className={({ isActive }) => (isActive ? "selected" : "not")}
+                >
+                  {v.name}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </HeaderCss>
+  );
+};
+export default Header;
