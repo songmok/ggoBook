7a48d6d675dd6d6d805dbf0331c75686547f5a82
devking <brand0610@naver.com>
2023-02-15 12:52:06 +0900

    login / signup axios 추가

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,3 @@
-
-import React from "react";
 import Form from "components/common/form/Form";
 import { useNavigate } from "react-router-dom";
 import LoginCss from "./style/LoginCss";
@@ -7,30 +5,26 @@ import kakao from "../../assets/images/kakao.png";
 import naver from "../../assets/images/naver.png";
 
 const Login = () => {
-  const navigte = useNavigate();
-
-
-  const goLogin = () => {
-    navigte("/login");
-  };
+  const navigate = useNavigate();
 
   const goSignup = () => {
-    navigte("/signup");
+    navigate("/signup");
   };
 
   return (
     <LoginCss>
-      <div>
-        <p>project_SB</p>
-        <p>한스의 캘린더</p>
+      <div className="title">
+        <p className="logo">Project_SB</p>
+        <p className="slogan">한스의 캘린더</p>
       </div>
-      <div>
-        <span onClick={goLogin}>로그인</span> or
+      <div className="orBt">
+        <span className="on">로그인</span>
+        <span> 또는 </span>
         <span onClick={goSignup}>등록</span>
       </div>
       <Form />
-      <div>
-        <span className="line">OR</span>
+      <div className="lineBox">
+        <span className="or"> 또는 </span>
       </div>
       <button className="kakao">
         <img src={kakao} alt="카카오 로그인" />
