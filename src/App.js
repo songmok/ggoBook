09673c3d63a92d523b151a101823aafa90f8cf04
songmok <www.songmokseed@gmail.com>
2023-02-16 18:06:28 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -11,26 +11,24 @@ import NotFound from "pages/notFound/NotFound";
 import Rank from "pages/rank/Rank";
 import Signup from "pages/signup/Signup";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import store from "reduser/store";
-import { Provider } from "react-redux";
+
 function App() {
   return (
     <>
       <Router>
         <Header />
-        <Provider store={store}>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/mycalendar" element={<MyCalendar />} />
-            <Route path="/event" element={<Event />} />
-            <Route path="/myPage" element={<MyPage />} />
-            <Route path="/rank" element={<Rank />} />
-            <Route path="/book" element={<Book />} />
-            <Route path="/bookdetail" element={<BookDetail />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Provider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/mycalendar" element={<MyCalendar />} />
+          <Route path="/event" element={<Event />} />
+          <Route path="/myPage" element={<MyPage />} />
+          <Route path="/rank" element={<Rank />} />
+          <Route path="/book" element={<Book />} />
+          <Route path="/bookdetail" element={<BookDetail />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
         <Footer />
       </Router>
     </>
