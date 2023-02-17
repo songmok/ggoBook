0b56d585f4dbdbd43273393423c075505be6574c
songmok <www.songmokseed@gmail.com>
2023-02-17 18:22:56 +0900

    header / 반응형 추가

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Footer from "components/common/footer/Footer";
 import Header from "components/common/header/Header";
 import Book from "pages/book/Book";
 import BookDetail from "pages/bookDetail/BookDetail";
-
 import Event from "pages/event/Event";
 import Login from "pages/login/Login";
 import MyCalendar from "pages/myCalendar/MyCalendar";
@@ -12,27 +11,30 @@ import NotFound from "pages/notFound/NotFound";
 import Rank from "pages/rank/Rank";
 import Signup from "pages/signup/Signup";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import theme from "theme/ThemeProvider";
 
 function App() {
   return (
     <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/mycalendar" element={<MyCalendar />} />
-          <Route path="/event" element={<Event />} />
-          <Route path="/myPage" element={<MyPage />} />
-          <Route path="/complete" element={<Complete />} />
-          <Route path="/rank" element={<Rank />} />
-          <Route path="/book" element={<Book />} />
-          <Route path="/bookdetail" element={<BookDetail />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-
-        <Footer />
-      </Router>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/mycalendar" element={<MyCalendar />} />
+            <Route path="/event" element={<Event />} />
+            <Route path="/myPage" element={<MyPage />} />
+            <Route path="/complete" element={<Complete />} />
+            <Route path="/rank" element={<Rank />} />
+            <Route path="/book" element={<Book />} />
+            <Route path="/bookdetail" element={<BookDetail />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <Footer />
+        </Router>
+      </ThemeProvider>
     </>
   );
 }
