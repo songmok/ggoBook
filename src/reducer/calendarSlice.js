20641f95ed43732c4ab3380336bce01990051fb9
songmok <www.songmokseed@gmail.com>
2023-02-20 09:42:44 +0900

    수정

--- src/reducer/calendarSlice.js
+++ src/reducer/calendarSlice.js
@@ -1,21 +1,52 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
+import { ADD_APPOINTMENT, SEARCH_APPOINTMENT } from "./appointmentTypes";
 
 const initialState = {
-  appointments: [],
+  appointments: localStorage.getItem("appointments")
+    ? JSON.parse(localStorage.getItem("appointments"))
+    : [],
   selectedAppointment: {},
 };
+
+// const appointmentReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case ADD_APPOINTMENT:
+//       return {
+//         ...state,
+//         appointments: [...state.appointments, action.payload],
+//       };
+//     case SEARCH_APPOINTMENT:
+//       return {
+//         ...state,
+//         selectedAppointment: state.appointments.find(
+//           (item) => item.id === action.payload
+//         ),
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// export default appointmentReducer;
 
 const appointmentReducer = createSlice({
   name: "calendar",
   initialState,
   reducers: {
     addCalendar(state, action) {
-      // return;{...state, calendarInfo: payload.data}
-      state.appointments = action.payload.data;
+      // const df = (state.appointments = action.payload.data);
     },
     selectedAppointment(state, action) {
-      state.selectedAppointment = action.payload.data;
+      // return {
+      //   ...state,
+      //   selectedAppointment: state.appointments.find(
+      //     (item) => item.id === action.payload
+      //   ),
+      // };
+      state.selectedAppointment = state.appointments.find(
+        (v) => v.id === action.payload
+      );
     },
   },
 });
--- src/reducer/store.ts
+++ src/reducer/store.ts
@@ -9,5 +9,6 @@ export const store = configureStore({
   },
 });
 
+export default store;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
