6c99fadcb48eeec09122939b157b04ddf7d8ff30
Banjae <brand0610@naver.com>
2023-02-20 18:24:19 +0900

    교보문고 api 연동

--- src/api/request.js
+++ src/api/request.js
@@ -1,6 +1,7 @@
 const request = {
   join: "api/user/join",
   login: "api/user/login",
+  sns_login: "/api/user/sns/login",
 };
 
 export default request;
--- src/components/book/BookSearch.js
+++ src/components/book/BookSearch.js
@@ -1,17 +1,38 @@
-import React from "react";
+import axios from "axios";
+import { TTBKey } from "OAuth";
+import React, { useState } from "react";
 
-const BookSearch = () => {
+const BookSearch = ({ setSearchResult }) => {
+  const [search, setSearch] = useState("");
+
+  const enterKey = (e) => {
+    e.key === "Enter" &&
+      axios({
+        method: "get",
+        url: "aladin/ttb/api/ItemSearch.aspx",
+        params: {
+          TTBKey: TTBKey,
+          Query: search,
+          MaxResults: 30,
+          Output: "JS",
+          Version: "20131101",
+        },
+      })
+        .then((res) => setSearchResult(res.data.item))
+        .catch((err) => console.log(err));
+  };
+
   return (
     <>
       <input
         type="text"
         className="search"
         placeholder="Type your message here"
-        //   value={word}
         onChange={(e) => {
-          // setWord(e.target.value);
+          setSearch(e.target.value);
         }}
-        //   onKeyPress={(e) => searchResult(e)}
+        value={search}
+        onKeyDown={enterKey}
       />
     </>
   );
