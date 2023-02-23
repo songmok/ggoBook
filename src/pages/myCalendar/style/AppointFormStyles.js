2470599c9ff7f156b293e48c30d08d74b789dc90
songmok <www.songmokseed@gmail.com>
2023-02-23 15:27:50 +0900

    캘린더 css 수정

--- src/pages/myCalendar/style/AppointFormStyles.js
+++ src/pages/myCalendar/style/AppointFormStyles.js
@@ -1,45 +1,33 @@
 import styled from "styled-components";
 import { Button } from "./CalendarStyles";
 
-export const FormWrapper = styled.div`
+export const FormCss = styled.article`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  .ReactModal__Content ReactModal__Content--after-open{
-    
+  h3 {
+    margin: 0 auto;
   }
-`;
-
-export const Header = styled.h3`
-  margin: 0 auto;
-`;
-
-export const ListItem = styled.li`
-  display: flex;
-  flex-direction: column;
-  padding: 0.3rem;
-`;
-
-export const ListContainer = styled.ul`
-  padding: 0;
-  list-style-type: none;
-  width: 100%;
-`;
-
-export const AddButton = styled(Button)`
-  background-color: #28b463;
-`;
-
-export const Input = styled.input`
-  padding: 0.3rem;
-  border: 0.1rem #c0c0c0 solid;
-`;
-
-export const Label = styled.label`
-  padding-bottom: 0.2rem;
-`;
-
-export const genderContainer = styled.div`
-  display: flex;
-  flex-direction: row;
+  form {
+  }
+  ul {
+    padding: 0;
+    width: 100%;
+    li {
+      display: flex;
+      flex-direction: column;
+      padding: 30px;
+      input {
+        padding: 30px;
+        border: 1px #c0c0c0 solid;
+      }
+      label {
+        padding-bottom: 20px;
+      }
+      /* datapicker{} */
+      button {
+        display: none;
+      }
+    }
+  }
 `;
